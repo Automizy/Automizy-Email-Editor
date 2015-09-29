45640a3122ecd76b74455e533d7691d9909e099e
Réti György <gyorgy.reti@protopmail.com>
2015-09-29 16:14:09 +0200

    update

--- php/sendhtml.php
+++ php/sendhtml.php
@@ -25,6 +25,8 @@ try {
 	$responseBody = $response->getBody();
 	$result = json_decode($responseBody->getContents(), JSON_OBJECT_AS_ARRAY);
 
+    var_dump($result);
+
 	$response = $client->get('contacts?'.\GuzzleHttp\Psr7\build_query([
             'where' => [
                 [
