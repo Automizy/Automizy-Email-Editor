30b2bf38e4b04123ab2e655272a4bd66f1c31906
Réti György <gyorgy.reti@protopmail.com>
2016-04-11 14:17:49 +0200

    update

--- www/js/editorsettings.js
+++ www/js/editorsettings.js
@@ -162,18 +162,18 @@ $(function(){
 		$AEE.open(250);
 	});
 
-    function getParameterByName(name, url) {
-        if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
-    }
-
     if(typeof getParameterByName('autostart') !== 'undefined' && getParameterByName('autostart') == 1){
         $AEE.open(250);
     }
 
 });
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
