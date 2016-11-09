ab546db1796f88ad404bd7a5a472830d540deac1
Réti György <gyorgy.reti@protopmail.com>
2016-11-09 16:53:20 +0100

    Example fix

--- dist/automizy-email-editor.js
+++ dist/automizy-email-editor.js
@@ -3096,19 +3096,21 @@
 })();
 
 (function(){
-    window.automizyHasMouse = false;
-        var mouseMoveListener = function () {
-            window.automizyHasMouse = true;
-            document.removeEventListener('mousemove', mouseMoveListener, false);
-        };
-        document.addEventListener('mousemove', mouseMoveListener, false);
+    window.automizyHasMouse = true;
 
-        $AEE.touchable = function () {
-            if (window.automizyHasMouse) {
-                return false;
-            }
-            return !!('ontouchstart' in window);
-        };
+    var mouseMoveListener = function () {
+        window.automizyHasMouse = true;
+        document.removeEventListener('mousemove', mouseMoveListener, false);
+    };
+    document.addEventListener('mousemove', mouseMoveListener, false);
+
+    $AEE.touchable = function () {
+        if (window.automizyHasMouse) {
+            return false;
+        }
+        return !!('ontouchstart' in window);
+    };
+    
 })();
 
 (function(){
